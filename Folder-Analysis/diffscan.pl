fb4c37493da3fa63d8f3b94ecd2939010fefb1e7
Jörg Meyer <joerg.meyer@jogisoft.de>
2023-01-03 13:42:17 +0100

    Add files via upload

---
+++ Folder-Analysis/diffscan.pl
@@ -0,0 +1,276 @@
+#!/usr/bin/env perl
+# (c) Joerg Meyer @ Jogisoft, 2005-01-05, 2010-02-06, 2022-12-26, 2023-01-03
+# Code copyrighted and shared under GPL v3.0
+
+# require cwd;
+
+$PROGRAM = 'diffscan.pl';
+$VERSION = 'v0.87';
+$DESCRPT = 'Verzeichnisbaeume vergleichen.';
+
+$trenn   = ' - ';
+$dopp    = ': ';
+$PathSep = "\\";
+
+($ENV{'PATH'} =~ m|/|) && ($PathSep = '/');
+
+$MODUS_REF  = 0;
+$MODUS_DIFF = 1;
+
+$AUSG_LISTE = 0;
+$AUSG_NAME  = 1;
+
+$IDENT_NAME = 0;
+$IDENT_ONLY = 1;
+$IDENT_TYP  = 2;
+$IDENT_CRC  = 3;
+
+$ONEDAY  = 24 * 3600;
+
+$diffcount = $errorcount = $filecount = $dircount = 0;
+$tiefe = $AUSFUEHRL = $STUMM = $Modus = $Ausgabe = $Ident = $Ignorecase = 0;
+$AuswTiefe = -1;
+$NextArgIsListFile = 0;
+$Orig = '';
+@Pfade = ();
+
+if (@ARGV < 2 and $ARGV[0] ne '-h' and $ARGV[0] ne '-v') {
+    print $PROGRAM, $dopp, "Keine Objekte angegeben.\n";
+    exit;
+}
+
+sub usage {
+    print 'Usage: ', $PROGRAM, " [Parameter] Originalpfad Vergleichsobjekt[e]\n\n",
+          "Parameter:\n",
+          "\t-0\tKeine Unterverzeichnisse auswerten\n",
+          "\t-1\tNur ein Verzeichnis tief auswerten\n",
+          "\t-2\tZwei Verzeichnisse tief auswerten\n",
+          "\t-3\tDrei Verzeichnisse tief auswerten\n",
+          "\t-4\tVier Verzeichnisse tief auswerten\n",
+          "\t-a\tAusfuehrliche Meldungen\n",
+          "\t-c\tIdent: Groesse, SDBM-Hash\n",
+          "\t-d\tAusgabe: Nur Dateinamen mit Pfad\n",
+          "\t-f\tVergleichsobjekte aus Datei einlesen\n",
+          "\t-g\tGross-/Kleinschreibung ignorieren\n",
+          "\t-h\tHilfeseite anzeigen\n",
+          "\t-i\tAlle Unterverzeichnisse auswerten (Vorgabe)\n",
+          "\t-l\tAusgabe: Liste mit Dateien (Vorgabe)\n",
+          "\t-n\tIdent: Name, Groesse, Zeitstempel (Vorgabe)\n",
+          "\t-o\tIdent: Nur Name und Typ\n",
+          "\t-q\tKeine Textmeldungen\n",
+          "\t-t\tIdent: Typ, Groesse, Zeitstempel\n",
+          "\t-v\tVersion anzeigen\n";
+    exit;
+}
+
+sub version {
+    print $PROGRAM, $trenn, $VERSION, "\n",
+          $DESCRPT, "\n";
+    exit;
+}
+
+foreach (@ARGV) {
+    if (substr($_, 0, 1) eq '-') {
+        m/0/ && ($AuswTiefe = 0);
+        m/1/ && ($AuswTiefe = 1);
+        m/2/ && ($AuswTiefe = 2);
+        m/3/ && ($AuswTiefe = 3);
+        m/4/ && ($AuswTiefe = 4);
+        m/a/ && ($AUSFUEHRL = 1);
+        m/c/ && ($Ident = $IDENT_CRC);
+        m/d/ && ($Ausgabe = $AUSG_NAME);
+        m/f/ && ($NextArgIsListFile = 1);
+        m/g/ && ($Ignorecase = 1);
+        m/h|\?/ && &usage();
+        m/i/ && ($AuswTiefe = -1);
+        m/l/ && ($Ausgabe = $AUSG_LISTE);
+        m/n/ && ($Ident = $IDENT_NAME);
+        m/o/ && ($Ident = $IDENT_ONLY);
+        m/q/ && ($STUMM = 1);
+        m/t/ && ($Ident = $IDENT_TYP);
+        m/v/ && &version();
+    }
+    else {
+        if ($Orig eq '') {
+            $Orig = $_;
+        }
+        else {
+            if ($NextArgIsListFile) {
+                if(open FILE, '<' . $_) {
+                    while (<FILE>) {
+                        chomp;
+                        next if /^$/;
+                        next if /^#/;
+                        push @Pfade, $_;
+                    }
+                    close FILE;
+                }
+                else {
+                    die "Kann Listendatei $_ nicht einlesen.\n";
+                }
+                $NextArgIsListFile = 0;
+            }
+            else {
+                push @Pfade, $_;
+            }
+        }
+    }
+}
+
+sub ScanFile {
+    my $eintrag = shift;
+
+    $filecount++;
+
+    if ($Ident == $IDENT_NAME or $Ident == $IDENT_ONLY) {
+        $id = $Ignorecase ? lc $eintrag : $eintrag;
+    }
+    elsif ($Ident == $IDENT_TYP) {
+        $id = $eintrag;
+        ($eintrag =~ /.*\.([^.]*)$/) && ($id = $1);
+    }
+    elsif ($Ident == $IDENT_CRC) {
+        $id = 0;
+
+        if (open FILE, '<' . $eintrag) {
+
+            # SDBM Algorithmus
+            while (read(FILE, $t, 4) == 4) {
+                $id = (($id << 16) + ($id << 6) - $id + unpack('I', $t));
+            }
+            close FILE;
+        }
+    }
+
+    if ($Ident == $IDENT_ONLY) {
+        $id = $eintrag;
+    }
+    elsif ($Ident == $IDENT_CRC) {
+        $id = join(':', -s $eintrag,
+                        $id);
+    }
+    else {
+        $id = join(':', -s $eintrag,
+                        int(($^T - $ONEDAY * (-M _)) / 60),
+                        $id);
+    }
+
+    if ($Modus == $MODUS_REF) {
+        $filearch{$id} = $AktPath . ($AktPath gt '' ? $PathSep : '') . $eintrag;
+    }
+    else {
+        if (exists $filearch{$id}) {
+            $filearch{$id} = '*';
+        }
+    }
+}
+
+sub ScanDir {
+
+    opendir VERZ, '.';
+    my @liste = readdir VERZ;
+    closedir VERZ;
+    $dircount++;
+
+    $AktPath = join($PathSep, @dirstack);
+    foreach $eintrag (@liste) {
+        &ScanFile($eintrag) if -f $eintrag;
+    }
+
+    return 2 if $AuswTiefe == $tiefe;
+
+    foreach $eintrag (@liste) {
+        next if $eintrag eq '.' or
+                $eintrag eq '..';
+
+        if (-d $eintrag) {
+
+            push @dirstack, $eintrag;
+
+            if (chdir $eintrag) {
+                $tiefe++;
+                &ScanDir;
+                $tiefe--;
+                chdir '..';
+            }
+            else {
+                print "Verzeichnis $eintrag nicht zugaenglich!.\n";
+                $errorcount++;
+            }
+            pop @dirstack;
+        }
+    }
+    return 1;
+}
+
+print $DESCRPT, "\n",
+      '-' x 75, "\n"
+    unless $Ausgabe == $AUSG_NAME;
+
+$Modus = $MODUS_REF;
+
+$Orig =~ s/(\/|\\)+$//;
+
+print "Lade Originaldateien aus: ", $Orig, "...\n" if $AUSFUEHRL;
+chdir $Orig || die $PROGRAM, $dopp, "Kann Pfad nicht finden!\n";
+&ScanDir;
+chdir '..';
+
+print "\t", $filecount, ' Dateien in ',
+            $dircount, " Verzeichnissen eingelesen.\n",
+            '-' x 75, "\n"
+    if $AUSFUEHRL;
+
+$Modus = $MODUS_DIFF;
+
+$StartPfad = $ENV{'PWD'};
+
+foreach $pfad (@Pfade) {
+
+    $pfad =~ s/(\/|\\)+$//;
+    print "Untersuche: ", $pfad, "...\n" if $AUSFUEHRL;
+
+    if ($StartPfad) {
+        chdir $StartPfad || die $PROGRAM, $dopp, "Kann Startpfad nicht finden!\n";
+    }
+
+    if (-f $pfad) {
+        &ScanFile($pfad);
+    }
+    elsif (-d _) {
+        chdir $pfad || die $PROGRAM, $dopp, "Kann Pfad nicht untersuchen!\n";
+        &ScanDir;
+        chdir '..';
+    }
+    else {
+        print $AUSFUEHRL ? "\tWas ist das?\n" : $PROGRAM . $dopp . $pfad . " nicht gefunden.\n";
+    }
+}
+print '-' x 75, "\n" if $AUSFUEHRL;
+
+foreach $t (keys %filearch) {
+    if ($filearch{$t} ne '*') {
+        $diffcount++;
+
+        $AktDat = $Orig . $PathSep . $filearch{$t};
+
+        if ($Ausgabe == $AUSG_LISTE) {
+            print 'Es fehlt: ', $AktDat, "\n" unless $STUMM;
+        }
+        else {
+            if ($PathSep eq '/') {
+                $AktDat =~ s/([ '])/\\\1/g;
+            }
+            else {
+                ($AktDat =~ m/ /) && ($AktDat = '"' . $AktDat . '"');
+            }
+            print $AktDat, "\n";
+        }
+    }
+}
+
+print '-' x 75, "\n",
+      'Fertig nach ', $diffcount, ' fehlenden Dateien in ',
+                      scalar(@Pfade), " Vergleichsobjekt(en).\n"
+    unless $Ausgabe == $AUSG_NAME;
+
---
+++ Folder-Analysis/dupescan.pl
@@ -0,0 +1,301 @@
+#!/usr/bin/env perl
+# (c) Joerg Meyer @ Jogisoft, 2005-01-05, 2010-02-07, 2022-12-26
+# Code copyrighted and shared under GPL v3.0
+
+$PROGRAM = 'dupescan.pl';
+$VERSION = 'v0.87';
+$DESCRPT = 'Dateidubletten auflisten.';
+
+$trenn   = ' - ';
+$dopp    = ': ';
+$PathSep = "\\";
+
+($ENV{'PATH'} =~ m|/|) && ($PathSep = '/');
+
+$AUSG_LISTE = 0;
+$AUSG_DOS   = 1;
+$AUSG_UNIX  = 2;
+$AUSG_NAME  = 3;
+
+$IDENT_NAME = 0;
+$IDENT_ONLY = 1;
+$IDENT_TYP  = 2;
+$IDENT_CRC  = 3;
+
+$ONEDAY  = 24 * 3600;
+
+$dupecount = $errorcount = $filecount = $dircount = $exclcount = 0;
+$tiefe = $AUSFUEHRL = $STUMM = $Ausgabe = $Ident = $Ignorecase = $ZeigStat = 0;
+$AuswTiefe = -1;
+$NextArgListFile = $NextArgAusschlListe = 0;
+@Pfade = ();
+$AuschlussListe = '';
+
+sub usage {
+    print 'Usage: ', $PROGRAM, " [Parameter] Objekt[e]\n\n",
+          "Parameter:\n",
+          "\t-0\tKeine Unterverzeichnisse auswerten\n",
+          "\t-1\tNur ein Verzeichnis tief auswerten\n",
+          "\t-2\tZwei Verzeichnisse tief auswerten\n",
+          "\t-3\tDrei Verzeichnisse tief auswerten\n",
+          "\t-4\tVier Verzeichnisse tief auswerten\n",
+          "\t-a\tAusfuehrliche Meldungen\n",
+          "\t-b\tAusgabe: DOS-Batchdatei\n",
+          "\t-c\tIdent: Groesse, SDBM-Hash\n",
+          "\t-d\tAusgabe: Nur Dateinamen mit Pfad\n",
+          "\t-e\tListe auszuschliessender Dateinamen\n",
+          "\t-f\tDateinamen bzw. Pfade aus Datei einlesen\n",
+          "\t-g\tGross-/Kleinschreibung ignorieren\n",
+          "\t-h\tHilfeseite anzeigen\n",
+          "\t-i\tAlle Unterverzeichnisse auswerten (Vorgabe)\n",
+          "\t-l\tAusgabe: Liste mit Dubletten (Vorgabe)\n",
+          "\t-n\tIdent: Name, Groesse, Zeitstempel (Vorgabe)\n",
+          "\t-o\tIdent: Nur Name und Typ\n",
+          "\t-q\tKeine Textmeldungen\n",
+#         "\t-e\tDateien mittels RegExps ausschliessen\n",
+          "\t-s\tStatistikinformationen inkl. Pfadnamen\n",
+          "\t-t\tIdent: Typ, Groesse, Zeitstempel\n",
+          "\t-u\tAusgabe: Unix-Shellskript\n",
+          "\t-v\tVersion anzeigen\n";
+    exit;
+}
+
+sub version {
+    print $PROGRAM, $trenn, $VERSION, "\n",
+          $DESCRPT, "\n";
+    exit;
+}
+
+if (@ARGV == 0) {
+    print $PROGRAM, $dopp, "Keine Objekte angegeben.\n\n";
+    &usage();
+}
+
+foreach (@ARGV) {
+    if (substr($_, 0, 1) eq '-') {
+        m/0/ && ($AuswTiefe = 0);
+        m/1/ && ($AuswTiefe = 1);
+        m/2/ && ($AuswTiefe = 2);
+        m/3/ && ($AuswTiefe = 3);
+        m/4/ && ($AuswTiefe = 4);
+        m/a/ && ($AUSFUEHRL = 1);
+        m/b/ && ($Ausgabe = $AUSG_DOS) && ($PathSep = '\\');
+        m/c/ && ($Ident = $IDENT_CRC);
+        m/d/ && ($Ausgabe = $AUSG_NAME);
+        m/e/ && ($NextArgAusschlListe = 1);
+        m/f/ && ($NextArgListFile = 1);
+        m/g/ && ($Ignorecase = 1);
+        m/h|\?/ && &usage();
+        m/i/ && ($AuswTiefe = -1);
+        m/l/ && ($Ausgabe = $AUSG_LISTE);
+        m/n/ && ($Ident = $IDENT_NAME);
+        m/o/ && ($Ident = $IDENT_ONLY);
+        m/q/ && ($STUMM = 1);
+        m/s/ && ($ZeigStat = 1);
+        m/t/ && ($Ident = $IDENT_TYP);
+        m/u/ && ($Ausgabe = $AUSG_UNIX) && ($PathSep = '/');
+        m/v/ && &version();
+    }
+    elsif ($NextArgAusschlListe) {
+        $ExclList = ',' . $_ . ',';
+        $NextArgAusschlListe = 0;
+    }
+    elsif ($NextArgListFile) {
+        if(open FILE, '<' . $_) {
+            while (<FILE>) {
+                chomp;
+                next if /^$/;
+                next if /^#/;
+                push @Pfade, $_;
+            }
+            close FILE;
+        }
+        else {
+            die "Kann Listendatei $_ nicht einlesen.\n";
+        }
+        $NextArgListFile = 0;
+    }
+    else {
+        push @Pfade, $_;
+    }
+}
+
+sub ScanFile {
+    my $eintrag = shift;
+
+    if (index($ExclList, ','.$eintrag.',') >= $[) {
+        $exclcount++;
+        return 2;
+    }
+
+    $filecount++;
+    $AktDat = $AktPath . $PathSep . $eintrag;
+
+    if ($Ident == $IDENT_NAME or $Ident == $IDENT_ONLY) {
+        $id = $Ignorecase ? lc $eintrag : $eintrag;
+    }
+    elsif ($Ident == $IDENT_TYP) {
+        $id = $eintrag;
+        ($eintrag =~ /.*\.([^.]*)$/) && ($id = $1);
+    }
+    elsif ($Ident == $IDENT_CRC) {
+        $id = 0;
+
+        if (open FILE, '<' . $eintrag) {
+
+            # SDBM Algorithmus
+            while (read(FILE, $t, 4) == 4) {
+                $id = (($id << 16) + ($id << 6) - $id + unpack('I', $t));
+            }
+            close FILE;
+        }
+    }
+
+    if ($Ident == $IDENT_ONLY) {
+        $id = $eintrag;
+    }
+    elsif ($Ident == $IDENT_CRC) {
+        $id = join(':', -s $eintrag,
+                        $id);
+    }
+    else {
+        $id = join(':', -s $eintrag,
+                        int(($^T - $ONEDAY * (-M _)) / 60),
+                        $id);
+    }
+
+    if (exists $filearch{$id}) {
+        $dupecount++;
+
+        if ($Ausgabe == $AUSG_DOS) {
+            $AktDat =~ tr/ÄÖÜäöüß/Ž™š„”á/;
+            print OUT 'del /Q "', $AktDat, '"', "\n";
+        }
+        elsif ($Ausgabe == $AUSG_UNIX) {
+            print OUT 'rm -f ', $AktDat, "\n";
+        }
+        elsif ($Ausgabe == $AUSG_NAME) {
+            if ($PathSep eq '/') {
+                $AktDat =~ s/([ '])/\\\1/g;
+            }
+            else {
+                ($AktDat =~ m/ /) && ($AktDat = '"' . $AktDat . '"');
+            }
+            print $AktDat, "\n";
+        }
+        else {
+            print '-' x 75, "\n" unless $STUMM;
+            print $filearch{$id}, "\n";
+            print $AktDat, "\n" unless $STUMM;
+        }
+
+        print "\t", $AktDat, "\n"
+            if ($Ausgabe > $AUSG_LISTE) && ($Ausgabe < $AUSG_NAME) && ($AUSFUEHRL);
+    }
+    else {
+        $filearch{$id} = $AktDat;
+    }
+
+}
+
+sub ScanDir {
+
+    opendir VERZ, '.';
+    my @liste = readdir VERZ;
+    closedir VERZ;
+    $dircount++;
+
+    $AktPath = join($PathSep, $pfad, @dirstack);
+
+    $ZeigStat && print $AktPath, $PathSep, "\n";
+
+    foreach $eintrag (@liste) {
+        &ScanFile($eintrag) if -f $eintrag;
+    }
+
+    return 2 if $AuswTiefe == $tiefe;
+
+    foreach $eintrag (@liste) {
+        next if $eintrag eq '.' or
+                $eintrag eq '..';
+
+        if (-d $eintrag) {
+
+            push @dirstack, $eintrag;
+
+            if (chdir $eintrag) {
+                $tiefe++;
+                &ScanDir;
+                $tiefe--;
+                chdir '..';
+            }
+            else {
+                print "Verzeichnis $eintrag nicht zugaenglich!.\n";
+                $errorcount++;
+            }
+            pop @dirstack;
+        }
+    }
+    return 1;
+}
+
+print $DESCRPT, "\n"
+    unless ($Ausgabe == $AUSG_NAME) or $STUMM;
+
+print 'Gebe Dubletten in ',
+      ($Ausgabe ? 'Loeschskript' : 'Listenform'),
+      " aus.\n"
+      if $AUSFUEHRL and $Ausgabe != $AUSG_NAME;
+
+if ($Ausgabe == $AUSG_DOS) {
+    open(OUT, '>dupes.bat');
+    print OUT "REM Dubletten loeschen\n";
+}
+elsif ($Ausgabe == $AUSG_UNIX) {
+    open(OUT, '>dupes.sh');
+    print OUT "#!/bin/sh\n";
+}
+
+$StartPfad = $ENV{'PWD'};
+
+print 'Dateien wegen Namen ausschliessen: ', substr($ExclList, 1, -1) , "\n" if $ExclList gt ',';
+
+foreach $pfad (@Pfade) {
+
+    $pfad =~ s/(\/|\\)+$//;
+    print "\nUntersuche: ", $pfad, "...\n" if $AUSFUEHRL;
+
+    if (-f $pfad) {
+        &ScanFile($pfad);
+    }
+    elsif (-d _) {
+
+        if ($StartPfad) {
+            chdir $StartPfad || die $PROGRAM, $dopp, "Kann Startpfad nicht finden!\n";
+        }
+
+        chdir $pfad || die $PROGRAM, $dopp, "Kann Pfad nicht untersuchen!\n";
+        &ScanDir;
+        chdir '..';
+    }
+    else {
+        print $AUSFUEHRL ? "\tWas ist das?\n" : $PROGRAM . $dopp . $pfad . " nicht gefunden.\n";
+    }
+}
+
+print OUT "echo Fertig.\n";
+if ($Ausgabe == $AUSG_DOS) {
+    print OUT "pause\n";
+}
+elsif ($Ausgabe == $AUSG_UNIX) {
+    print OUT "read\n";
+}
+close(OUT) if $Ausgabe != 0;
+
+print '-' x 75, "\n",
+      'Fertig mit ', $dupecount, ' Dubletten in ',
+                     $filecount, ' Dateien in ',
+                     $dircount, ' Verzeichnissen in ',
+                     scalar(@Pfade), " Objekt(en).\n"
+    unless ($Ausgabe == $AUSG_NAME) or $STUMM;
+
