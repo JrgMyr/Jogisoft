e814bb7a0d81b5026b6cf8232887e40b36cfa794
Jörg Meyer <joerg.meyer@jogisoft.de>
2023-01-04 16:08:55 +0100

    Add files via upload

--- Sequence-Analysis/scanint.pl
+++ Sequence-Analysis/scanint.pl
@@ -1,31 +1,35 @@
 #!/usr/bin/env perl
-# (c) Joerg Meyer @ Jogisoft, 2008-12-11, 2010-01-12, 2023-01-03
+# (c) Joerg Meyer @ Jogisoft, 2008-12-11, 2010-01-12, 2023-01-04
 # Code copyrighted and shared under GPL v3.0
+
+$PROGRAM = 'scanint.pl';
+$VERSION = 'v0.31';
 
 print "Zahlenfolgen in Intervallen zusammenfassen.\n";
 exit if $ARGV[0] eq '-h';
 
 $s = '';
 $n = '';
-$l = 0;
 
 while (<>) {
-    $l++;
     chomp;
-
-    next unless /[0-9]+/;
+    next if /^ *$/;
+    if (/[^0-9]/) {
+        print 'Zeile ', $., ":\tNicht-numerischer Inhalt! '", $_, "'\n";
+        next;
+    }
 
     if ($s eq '') {
         $s = $_;
     }
     elsif ($n == $_) {
-        print "Zeile $l:\tWiederholung: $n.\n";
+        print 'Zeile ', $., ":\tWiederholung: ", $n, ".\n";
     }
     elsif ($n+1 != $_) {
-        print "Zeile $l:\tIntervall: $s bis $n.\n";
+        print 'Zeile ', $., ":\tIntervall: ", $s, ' bis ', $n, ".\n";
         $s = $_;
     }
     $n = $_;
 }
-print "Zeile $l:\tIntervall: $s bis $n.\n" if $s ne '';
-print "Ende nach $l Zeilen \n";
+print "Zeile $.:\tIntervall: $s bis $n.\n" if $s ne '';
+print "Ende nach $. Zeilen \n";
--- Sequence-Analysis/scanseq.pl
+++ Sequence-Analysis/scanseq.pl
@@ -1,29 +1,35 @@
 #!/usr/bin/env perl
-# (c) Joerg Meyer @ Jogisoft, 2008-12-11, 2015-10-26, 2023-01-03
+# (c) Joerg Meyer @ Jogisoft, 2008-12-11, 2015-10-26, 2023-01-04
 # Code copyrighted and shared under GPL v3.0
+
+$PROGRAM = 'scanseq.pl';
+$VERSION = 'v0.31';
 
 print "Zahlenfolgen auf Luecken pruefen.\n";
 exit if $ARGV[0] eq '-h';
 
 $n = '';
-$l = 0;
 
 while (<>) {
-    $l++;
     chomp;
-
-    next unless /[0-9]+/;
+    next if /^ *$/;
+    if (/[^0-9]/) {
+        print 'Zeile ', $., ":\tNicht-numerischer Inhalt! '", $_, "'\n";
+        next;
+    }
 
     if ($n eq '') {
-        print "Zeile $l:\tBeginn mit Wert $_.\n";
+        print "Zeile $.:\tBeginn mit Wert $_.\n";
     }
-    elsif ($n eq $_) {
-        print "Zeile $l:\tWiederholung $n.\n";
+    elsif ($_ eq $n) {
+        print "Zeile $.:\tWiederholung $n.\n";
     }
-    elsif ($n+1 != $_) {
-        print "Zeile $l:\tLuecke zwischen $n und $_.\n";
+    elsif ($_ < $n) {
+        print "Zeile $.:\tFalsche Reihenfolge mit Wert $_.\n";
+    }
+    elsif ($_ > $n+1) {
+        print "Zeile $.:\tLuecke zwischen $n und $_.\n";
     }
     $n = $_;
 }
-print "Ende nach $l Zeilen mit Wert $n.\n";
-
+print "Ende nach $. Zeilen mit Wert $n.\n";
